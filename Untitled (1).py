22f75b338774e2a8134c263926a4fefb40f8c79d
damini mukhi <49147491+daminimukhi@users.noreply.github.com>
2021-08-03 01:50:53 +0500

    Add files via upload

---
+++ Untitled (1).py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[19]:
+
+
+a='hey'
+b="bye"
+c='''"why?"'''
+d= 65
+e= 45.5
+f= True
+g= None
+print(type(a))
+print(a)
+print(b)
+print(c)
+print(type(d))
+print(d)
+print(type(e))
+print(e)
+print(type(f))
+print(f)
+print(type(g))
+print(g)
+
+
+# In[15]:
+
+
+# arithemetic operators
+print('''"arthimetic operator"''')
+a = 3
+b = 6
+print("addition",3+6)
+
+a = 3
+b = 6
+print("subtraction",3-6)
+
+a = 3
+b = 6
+print("multiplication",3*6)
+
+a = 3
+b = 6
+print("division",3/6)
+
+# assignment operators
+print('''"assignment operator"''')
+a = 4
+a += 2
+a -= 2
+a /= 2
+a *= 2
+print("addition,subtraction,multiplication,division",a)
+
+# comparison operator
+print('''"comparison operator"''')
+a = 6
+b = 7
+print(a < b)
+print(a > b)
+print(a == b)
+print(a != b)
+print(a >= b)
+print(a <= b)
+
+# logical operator
+print('''"logical operator"''')
+a = False
+b = True
+print(a and b)
+print(a or b)
+print(not b)
+print(not a)
+
+
+# In[7]:
+
+
+# Typecasting 
+print('''"typecasting"''')
+a = '34'
+b = 35.7
+c = 34
+print(int(a))
+print(int(b))
+print(str(c))
+print(float(c))
+
+
+# In[14]:
+
+
+# input from the user
+print("Taking input from the user")
+print(input("Enter your name: "))
+print(input("Enter your age: "))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
