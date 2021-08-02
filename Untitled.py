bb2053a236ed21241f0fa7cc7cef03ba3f6d2dfe
damini mukhi <49147491+daminimukhi@users.noreply.github.com>
2021-08-03 01:49:54 +0500

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,46 @@
+# Chapter-02
+# Practice set
+print("01. Write a Python program to add two numbers.")
+a = 34
+b = 44
+print("Sum of two numbers: ",a+b)
+print("")
+
+print("02. Write a Python program to find the remainder when a number is divided by Z(Integer)")
+a = 45
+b = 15
+print(a%b)
+print("")
+
+print("03. Check the type of the variable assigned using the input() function.")
+print(type(input("What is your name? ")))
+print("")
+
+
+print("04. Use a comparison operator to find out whether a given variable a is greater than b or not. (Take a=34 and b=80)")
+print("Comparison Operator")
+a = 34
+b = 80
+print("a is greater than b or not")
+print("Answer: ",a >= b)
+print("")
+
+print("05. Write a Python program to find the average of two numbers entered by the user.")
+a = input("Enter first Number: ")
+b = input("Enter second Number ")
+a = int(a)
+b = int(b)
+avg = (a+b)/2
+print("The value of average is ", avg)
+print("")
+
+print("06. Write a Python program to calculate the square of a number entered by the user.")
+a = input("Enter the Number to be squared: ")
+a = int(a)
+print("The Square: ", a*a)
+
+
+
+
+
+
